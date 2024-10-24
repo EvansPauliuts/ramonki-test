359cb189bafc2109e5f00b536da3dd2269ee6c5b
Evans <pauliutsevans@gmail.com>
2024-10-24 12:00:25 +0300

    Final test #2

--- pages/catalogPage.js
+++ pages/catalogPage.js
@@ -1,31 +1,3 @@
-// class CatalogPage {
-//     constructor(page) {
-//         this.page = page;
-//         this.locators = {
-//             clothesLink: '.drop-menu__link--catalog >> text="Одежда"',
-//             dressesLink: '.drop-menu__list--wrap >> .drop-menu__item >> .drop-menu__link >> text="Платья и сарафаны"',
-//             menu: '.drop-menu__menu--wide',
-//         };
-//     }
-//
-//     async selectClothes() {
-//         await this.page.locator(this.locators.clothesLink).click();
-//     }
-//
-//     async selectDresses() {
-//         await Promise.all([
-//             this.page.waitForNavigation(),
-//             this.page.locator(this.locators.dressesLink).click()
-//         ]);
-//     }
-//
-//     async waitForPage() {
-//         await this.page.waitForSelector(this.locators.menu, { timeout: 5000 });
-//     }
-// }
-//
-// module.exports = CatalogPage;
-
 class CatalogPage {
     constructor(page) {
         this.page = page;
